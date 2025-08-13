90ea75f7175227c07378c4d4685c7b155c7866e2
Rajas Paranjpe <52586855+ChocolateLoverRaj@users.noreply.github.com>
2025-08-12 17:08:51 -0700

    Add files from rust-os-tutorial

---
+++ README.md
@@ -0,0 +1,18 @@
+![Crates.io Version](https://img.shields.io/crates/v/ez_pci)
+![docs.rs](https://img.shields.io/docsrs/ez_pci)
+
+The goal of this library is to make it very easy to implement PCI in your own OS.
+
+## Features
+- PCI (I/O based) and PCIe (MMIO based)
+- Scan buses, devices, and functions
+- Read BAR address and size
+- Iterate through capabilities
+- Configure MSI
+
+## Planned
+- MSI-X
+- Better concurrent access
+
+This crate is still very new. If you notice any limitations with this crate have feature requests, or want help using this crate, feel free to open an issue on GitHub.
+ 
---
+++ src/lib.rs
@@ -0,0 +1,31 @@
+//! To use PCI, use [`PciAccess::new_pci`].
+//! To use PCIe, use [`PciAccess::new_pcie`].
+//!
+//! Then you can scan buses.
+//! For each bus, you can scan devices.
+//! For each device, you can scan functions.
+//! For each function, you can scan BARs, capabilities, and general info.
+//!
+//! You can also find and configure MSI (Message Signaled Interrupts)
+#![no_std]
+mod bar;
+mod bus;
+mod capabilities;
+mod device;
+mod function;
+mod get_phys_range_to_map;
+mod header_type;
+mod msi;
+mod pci_access;
+mod pci_config;
+
+pub use bar::*;
+pub use bus::*;
+pub use capabilities::*;
+pub use device::*;
+pub use function::*;
+pub use get_phys_range_to_map::*;
+pub use header_type::*;
+pub use msi::*;
+pub use pci_access::*;
+use pci_config::*;
